51ed9f8695ffeeb9fa66feb1b57bf8d51ce0bab2
james <382127999@qq.com>
2019-03-18 00:15:42 +0800

    first commit

--- catkin_ws/build/tiscamerasample/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/james/catkin_ws/src/tiscamerasample/src
-# Build directory: /home/james/catkin_ws/build/tiscamerasample/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
